4b264a6ca5f8dec8245e6e630c54b174b233658c
negoool <negar.iravanii@gmail.com>
2023-08-26 01:29:59 +0000

    adding the logging

--- text2speech/infer.py
+++ text2speech/infer.py
@@ -1,3 +1,6 @@
+import logging
+import time
+
 import torch
 from datasets import load_dataset
 from IPython.display import Audio
@@ -7,18 +10,23 @@ from transformers import (
     SpeechT5Processor,
 )
 
+from text2speech.logger_configure import get_logger
+
 
 class Infer:
     def __init__(self, model_name: str = "microsoft/speecht5_tts") -> None:
         """Responsible for making inference on a given text adn speaker id"""
         # Load models
+        logger.info("Loading the model and preprocessor")
         self.processor = SpeechT5Processor.from_pretrained(model_name)
         self.model = SpeechT5ForTextToSpeech.from_pretrained(model_name)
 
         # Load vovector
+        logger.info("Loading  vovector")
         self.vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
 
         # Load embeddings
+        logger.info("Loading embedding")
         self.embedding = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
 
     def tts(self, text: str, speaker_id: int) -> torch.Tensor:
@@ -31,5 +39,8 @@ class Infer:
         return Audio(waveform, rate=16000)
 
     def run(self, text: str, speacker_id: int = 16) -> None:
+        st = time.time()
         speech = self.tts(text, speacker_id)
-        self.listen_to_speach(speech)
+        infer_time = time.time() - st
+        logger.debug({"infer_time": infer_time, "text_length": len(text), "speech_length": round(speech.shape[0] / 16000, 2)})
+        # self.listen_to_speach(speech)
